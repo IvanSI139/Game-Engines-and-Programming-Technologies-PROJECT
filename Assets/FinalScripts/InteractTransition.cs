29bba6b8d0e6bf6eea07558f6a4e0cc014251377
IvanSI139 <dr.iliev.2000@gmail.com>
2024-12-20 10:42:44 +0100

    ????

---
+++ Assets/FinalScripts/InteractTransition.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InteractableObject : MonoBehaviour
+{
+    private bool playerInRange = false;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the player has entered the trigger
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Collision");
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        // Check if the player has exited the trigger
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        // Check if the player is in range and presses the "E" key
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            LoadNextScene();
+        }
+    }
+
+    void LoadNextScene()
+    {
+        // Get the current active scene's build index and load the next scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Check if the next scene index is within bounds
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No more scenes to load. End of build.");
+        }
+    }
+}
